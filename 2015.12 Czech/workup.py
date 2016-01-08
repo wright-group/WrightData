18bf871cdc21507bad7906b6229d158bb6dab1e5
Blaise <blaise@untzag.com>
2016-01-07 20:43:09 -0600

    python package creation
    Using 2015.12 Czech as a model to develop infrastructure for use as
    python package.

---
+++ 2015.12 Czech/workup.py
@@ -0,0 +1,74 @@
+### import ####################################################################
+
+
+import os
+import collections
+
+import WrightTools as wt
+
+
+### define ####################################################################
+
+
+directory = os.path.dirname(__file__)
+
+raw_dictionary = collections.OrderedDict()
+processed_dictionary = collections.OrderedDict()
+
+
+### movie #####################################################################
+
+
+def workup_movie():
+    # raw
+    data_paths = wt.kit.glob_handler('.dat', folder=os.path.join(directory, 'movie'))
+    raw_movie = wt.data.from_COLORS(data_paths, name='MoS2 TrEE Movie')
+    raw_movie.save('raw_movie.p')
+    # processed
+    processed_movie = raw_movie.copy()
+    processed_movie.level('ai0', 'd2', -3)
+    processed_movie.smooth([2, 2, 0], channel='ai0')
+    processed_movie.scale(channel='ai0', kind='amplitude')
+    processed_movie.normalize(channel='ai0')
+    processed_movie.save('processed_movie.p')
+    # finish
+    return raw_movie, processed_movie
+
+# get from pickle or create
+if os.path.isfile('raw_movie.p'):
+    raw_movie = wt.data.from_pickle('raw_movie.p', verbose=False)
+    processed_movie = wt.data.from_pickle('processed_movie.p', verbose=False)
+else:
+    raw_movie, processed_movie = workup_movie()
+
+# check version
+if not raw_movie.__version__.split('.')[0] == wt.__version__.split('.')[0]:
+    raw_movie, processed_movie = workup_movie()
+
+# add to dictionaries
+raw_dictionary['movie'] = raw_movie
+processed_dictionary['movie'] = processed_movie
+
+
+### absorbance ################################################################
+
+
+def workup_absorbance():
+    absorbance_path = os.path.join(directory, 'MoS2_TF_III_ebeam_1nm_Mo_onQuartz_T=300K__corrected.txt')
+    absorbance_data = wt.data.from_shimadzu(absorbance_path, name='MoS2 thin film absorbance')
+    absorbance_data.save('absorbance_data.p')
+    return absorbance_data
+    
+# get from pickle or create
+if os.path.isfile('absorbance_data.p'):
+    absorbance_data = wt.data.from_pickle('absorbance_data.p', verbose=False)
+else:
+    absorbance_data = workup_absorbance()
+    
+# check version
+if not absorbance_data.__version__.split('.')[0] == wt.__version__.split('.')[0]:
+    absorbance_data = workup_absorbance()
+    
+# add to dictionaries
+raw_dictionary['absorbance'] = absorbance_data
+processed_dictionary['absorbance'] = absorbance_data
---
+++ __init__.py
@@ -0,0 +1,40 @@
+''' 
+Handle getting of data objects from repository subfolders.
+'''
+
+### import ####################################################################
+
+
+import os
+import imp
+import collections
+
+
+### get method ################################################################
+
+
+def get(match_string, process=True):
+    # get all subfolder paths
+    d = os.path.dirname(__file__)
+    folders = [os.path.join(d,o) for o in os.listdir(d) if os.path.isdir(os.path.join(d,o)) and not '.git' in o]
+    # choose particular folder
+    matching_folders = []
+    for f in folders:
+        if match_string in f:
+            matching_folders.append(f)
+    if len(matching_folders) > 1:
+        print 'match_string {} matches multiple folders'.format(match_string)
+        return None
+    elif len(matching_folders) == 0:
+        print 'match_string {} does not match any folder'.format(match_string)
+        return None
+    else:
+        folder = matching_folders[0]
+    # get data dictionary
+    module = imp.load_source('workup', os.path.join(folder, 'workup.py'))
+    if process:
+        dictionary = module.processed_dictionary
+    else:
+        dictionary = module.raw_dictionary
+    # finish
+    return dictionary
